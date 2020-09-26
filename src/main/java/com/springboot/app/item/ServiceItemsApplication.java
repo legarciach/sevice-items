58cd8cf78a7f87061963da39a7ee27be5ae2688d
Enrique Garcia <enrique.escom@gmail.com>
2020-09-25 19:40:46 -0500

    commit inicial

---
+++ src/main/java/com/springboot/app/item/ServiceItemsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceItemsApplication.class, args);
+    }
+
+}
