789a135259c4dbe2e3384ab4407ff654914a71c4
Enrique Garcia <enrique.escom@gmail.com>
2020-09-28 14:07:04 -0500

    Tolerancia a fallos con Hystrix

--- src/main/java/com/springboot/app/item/ServiceItemsApplication.java
+++ src/main/java/com/springboot/app/item/ServiceItemsApplication.java
@@ -2,12 +2,14 @@ package com.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ServiceItemsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springboot/app/item/controlles/ItemController.java
+++ src/main/java/com/springboot/app/item/controlles/ItemController.java
@@ -1,6 +1,9 @@
 package com.springboot.app.item.controlles;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.springboot.app.item.models.Item;
+import com.springboot.app.item.models.Producto;
 import com.springboot.app.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +23,18 @@ public class ItemController {
         return service.listAll();
     }
 
+    @HystrixCommand(fallbackMethod = "alternativoVer")
     @GetMapping("/ver/id/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
         return service.findById(id, cantidad);
     }
+
+    public Item alternativoVer(Long id, Integer cantidad){
+        Producto producto = new Producto();
+        producto.setId(id);
+        producto.setNombre("Nuevo producto");
+        producto.setPrecio(999.5);
+        return new Item(producto, cantidad);
+    }
+
 }
